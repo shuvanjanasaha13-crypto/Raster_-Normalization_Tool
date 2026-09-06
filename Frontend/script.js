9e9f21ad8d1d563cd6394b2d8fbeffc6f0fc6957
shuvanjanasaha13-crypto <shuvanjanasaha13@gmail.com>
2026-09-06 23:33:58 +0600

    Update frontend and Render API

--- Frontend/script.js
+++ Frontend/script.js
@@ -56,7 +56,7 @@ normalizeBtn.addEventListener(
             const response =
             await fetch(
 
-                "http://127.0.0.1:5000/normalize",
+                "https://raster-normalization-tool-api-h5q6.onrender.com/normalize",
 
                 {
 
